069e763b5723216c235e36be68a40544e1c73465
Selina <aniles162@gmail.com>
2024-11-10 00:26:11 +0100

    tests done

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -14,16 +14,31 @@ class TestMathGame(unittest.TestCase):
 
     def test_function_B(self):
         # TODO
+        # Test if the operators are the correct symbols
+
+        operators = {'+', '-', '*'}
+        for _ in range(1000): # Test a large number of random values
+            op = get_operator() 
+            self.assertIn(op, operators) # test if one of the random chosen operators "op" is in the set of "operators"
+
         pass
 
     def test_function_C(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                
+                (3, 6, '*', '3 * 6', 18),
+                (7, 1, '-', '7 - 1', 6),
+                (4, 8, '+', '4 + 8', 12),
+                (2, 5, '*', '2 * 5', 10),
+                (9, 7, '-', '9 - 7', 2)
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
+                
+                problem, answer = gen_math_problem(num1, num2, operator)
+                self.assertEqual(problem, expected_problem)  
+                self.assertEqual(answer, expected_answer)   
                 pass
 
 if __name__ == "__main__":
