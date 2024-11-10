670848d3976c8906f7eaf72845df99aad3b1b680
aniles162 <aniles162@gmail.com>
2024-11-10 23:23:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='dsss_homework_2',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(include=['math_quiz', 'math_quiz.*']),  
     install_requires=[
-      
+        
     ],
-) 
+)
