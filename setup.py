1537d5cdd2c2dd19cc4ca6550788fcf490c7b209
aniles162 <aniles162@gmail.com>
2024-11-10 23:32:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dsss_homework_2',
-    version='0.1',
-    packages=find_packages(include=['math_quiz', 'math_quiz.*']),  
+    name='dsss_homework_2',  
+    version='0.1',           
+    packages=find_packages(include=['math_quiz', 'math_quiz.py', 'tests_math_quiz.py', '__init__.py']),  
     install_requires=[
         
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz.math_quiz:main',  
+        ],
+    },
+    include_package_data=True,  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
