e15e60c1daa270c2e3c299b54a91046f7ade5f98
aniles162 <aniles162@gmail.com>
2024-11-11 00:09:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="math_quiz",
     version="0.1.0",
-    description="Math_quiz",
+    description="math_quiz",
     author="Selina Werner",
     author_email="aniles162@gmail,com",
     url="https://github.com/aniles162/dsss_homework_2.git",
-    packages=find_packages(include=['math_quiz', 'math_quiz.*']),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
